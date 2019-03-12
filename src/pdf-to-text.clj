e4f09aa79146a718393553bd286efe72f638dbe2
Vee Satayamas <vsatayamas@gmail.com>
2019-03-12 18:05:10 +0700

    Close PDF Document

--- src/pdf-to-text.clj
+++ src/pdf-to-text.clj
@@ -7,15 +7,15 @@
 (defn pdf-to-text
   [f]
   (with-open [r (RandomAccessFile. f "r")]
-    (let [parser (PDFParser. r)
-          cos-doc (.getDocument parser)
-          pd-doc (PDDocument. cos-doc)
+    (let [parser (PDFParser. r)          
           text-stripper (PDFTextStripper.)]
-      (.parse parser)
-      (doto text-stripper
-        (.setStartPage 0)
-        (.setStartPage (.getNumberOfPages pd-doc)))
-      (.getText text-stripper pd-doc))))
+      (with-open [cos-doc (.getDocument parser)
+                  pd-doc (PDDocument. cos-doc)]
+        (.parse parser)
+        (doto text-stripper
+          (.setStartPage 0)
+          (.setStartPage (.getNumberOfPages pd-doc)))
+        (.getText text-stripper pd-doc)))))
 
 ;; Tutorial for Java
 ;; https://radixcode.com/pdfbox-example-code-how-to-extract-text-from-pdf-file-with-java
